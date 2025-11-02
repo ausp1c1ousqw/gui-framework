4c48bc43d1ee55fe3756c0b9264f0cca2321dcf0
Sergey Chernyavsky <qausp1c1ousw@gmail.com>
2025-11-02 18:56:56 +0300

    a

--- src/configs/hooks.js
+++ src/configs/hooks.js
@@ -1,40 +1,39 @@
 import allure from "@wdio/allure-reporter";
 import Screenshot from "../utils/Screenshot.js";
 import PageSource from "../utils/PageSource.js";
-import Framework from "../configs/Framework.js";
-const { logger } = Framework;
+import fw from "../configs/Framework.js";
 
 const hooks = {
   beforeScenario: async function (world) {
-    logger.info(`Scenario started: ${world.pickle.name}`);
+    fw.logger.info(`Scenario started: ${world.pickle.name}`);
   },
 
   beforeStep: async function (step) {
-    logger.info(`Step: ${step.text}`);
+    fw.logger.info(`Step: ${step.text}`);
   },
 
   afterStep: async function (step, scenario, { error }) {
-    logger.info(`Step ended: ${step.text}`);
+    fw.logger.info(`Step ended: ${step.text}`);
 
-    const stepLogs = logger.getLogs();
+    const stepLogs = fw.logger.getLogs();
     allure.addAttachment(`Logs for: ${step.text}`, stepLogs, "text/plain");
 
-    logger.clear();
+    fw.logger.clear();
   },
 
   afterScenario: async function (world, result) {
-    logger.info(`=== End Scenario: ${world.pickle.name} ===`);
+    fw.logger.info(`=== End Scenario: ${world.pickle.name} ===`);
   },
 
   onError: async function (error) {
-    logger.error(error);
+    fw.logger.error(error);
 
     const { screenshotPath, screenshot } = await Screenshot.take();
-    logger.info(`Screenshot path: ${screenshotPath}`);
+    fw.logger.info(`Screenshot path: ${screenshotPath}`);
     allure.addAttachment("Screenshot", Buffer.from(screenshot, "base64"), "image/png");
 
     const { pageSourcePath, pageSource } = await PageSource.get();
-    logger.info(`Page Source path: ${pageSourcePath}`);
+    fw.logger.info(`Page Source path: ${pageSourcePath}`);
     allure.addAttachment("Page source", pageSource, "text/html");
   },
 };
